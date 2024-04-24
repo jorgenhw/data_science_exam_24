a26f6a06a8fbd163b15f128bbfe39ece1d108f1a
jorgenhw <jrgenhwn1@gmail.com>
2024-04-24 08:51:23 +0200

    venv and requirements

---
+++ setup.sh
@@ -0,0 +1,8 @@
+python3 -m venv venv_data_science_exam
+
+#activate
+source ./venv_data_science_exam/bin/activate
+
+# Install requirements
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
