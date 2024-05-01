7bcba7aa4cc74069e1b81200cbe31589c8ae3aa3
jorgenhw <jrgenhwn1@gmail.com>
2024-05-01 09:54:50 +0200

    data prep

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
+# Check if Python 3.12 is installed
+if ! command -v python3.12 &> /dev/null
+then
+    echo "Python 3.12 could not be found. Please install it."
+    exit
+fi
+
 python3 -m venv venv_data_science_exam
 
 #activate
 source ./venv_data_science_exam/bin/activate
 
 # Install requirements
-python3 -m pip install --upgrade pip
+
 pip3 install -r requirements.txt
